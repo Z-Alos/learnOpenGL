d7cb15408b2a127c3053c05189078ba527c3b3b1
Zalos <satya123m4@gmail.com>
2025-07-27 10:22:09 +0530

    add: Phong++

--- Lighting/01_Colors/colors.fs
+++ Lighting/01_Colors/colors.fs
@@ -5,12 +5,15 @@ in vec3 Normal;
 in vec3 FragPos;
 
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
 
 void main(){
-    // ambient
     float ambientStrength=0.2;
+    float specularStrength=0.8;
+
+    // ambient
     vec3 ambient = ambientStrength*lightColor;
 
     // diffuse
@@ -19,7 +22,13 @@ void main(){
     float diff=max(dot(norm, lightDir), 0.0);
     vec3 diffuse=diff*lightColor;
 
-    vec3 result=(ambient+diffuse)*objectColor;
+    // specular
+    vec3 viewDir=normalize(viewPos-FragPos);
+    vec3 reflectDir=reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength*spec*lightColor;
+
+    vec3 result=(ambient+diffuse+specular)*objectColor;
     FragColor=vec4(result, 1.0);
 };
 
